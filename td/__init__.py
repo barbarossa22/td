4e06063c3d892a612e47af4df4c04b469a43b0f5
Mykola Radionov <mrad@rv790.softservecom.com>
2017-01-10 17:43:36 +0200

    Add project files and basic todo list realization with items savings in session memory.

---
+++ td/__init__.py
@@ -0,0 +1,23 @@
+from pyramid.config import Configurator
+from pyramid.session import SignedCookieSessionFactory
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+
+
+    config.add_static_view('static', path='/home/mrad/td/td/static',
+                          cache_max_age=3600)
+    # config.add_route('home', '/')
+    config.add_route('todo_list', '/todo_list')
+    config.add_route('get_todo_list_items', '/api/get_todo_list_items')
+    config.add_route('add_todo_list_item', '/api/add_todo_list_item')
+
+    my_session_factory = SignedCookieSessionFactory('super_secret')
+    #msf = SignedCookieSessionFactory()
+    config.set_session_factory(my_session_factory)
+
+    config.scan()
+    return config.make_wsgi_app()
---
+++ td/views.py
@@ -0,0 +1,52 @@
+from pyramid.view import view_config
+from pyramid.response import FileResponse, Response
+
+
+@view_config(route_name='todo_list', request_method='GET')
+def get_todo_list_page(request):
+    """Return web-page base.html on /todo_list url"""
+    return FileResponse('/home/mrad/td/td/static/base.html', cache_max_age=3600)
+
+
+@view_config(
+    route_name='get_todo_list_items',
+    renderer='json',
+    request_method='GET'
+)
+def get_todo_list_items(request):
+    """ Get list of items at /api/get_todo_list_items.
+
+    If there are notes in current session memory return JSON in this format:
+    {'items': ['sleep', 'eat', 'repeat']}
+    Else: {'items': null}
+    (in python code it looks like None, but later it'll be translated to JSON's
+     null)
+    """
+    session = request.session
+    if 'items' in session:
+        return {'items': session['items']}
+    else:
+        return {'items': None}
+
+
+@view_config(route_name='add_todo_list_item', request_method='POST')
+def add_todo_list_item(request):
+    """ Add item to the session memory at post request on
+    /api/add_todo_list_item
+    """
+    session = request.session
+    print 'Adding item "%s" to session.' % request.json_body['item']
+    # del session['items']
+    if 'items' in session:
+        session['items'].append(request.json_body['item'])
+    else:
+        session['items'] = []
+        session['items'].append(request.json_body['item'])
+    return Response('OK')
+
+
+'''
+@view_config(route_name='home', renderer='templates/mytemplate.pt')
+def my_view(request):
+    return {'project': 'td'}
+'''
